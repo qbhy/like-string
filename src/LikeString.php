bf844b27aa3d543859f77e4cd3e8bb781029b477
96qbhy <96qbhy@gmail.com>
2017-08-12 15:32:05 +0800

    添加根据格式生成数字字符串的函数

--- src/LikeString.php
+++ src/LikeString.php
@@ -13,11 +13,11 @@ class LikeString
     {
         $formatLen = strlen($format);
         $strLen = strlen($str);
+        $mapping = [];
         if ($formatLen !== $strLen) {
             return false;
         }
 
-        $mapping = [];
 
         for ($i = 0; $i < $strLen; $i++) {
             if (!isset($mapping[$str[$i]])) {
@@ -30,6 +30,50 @@ class LikeString
         return true;
     }
 
+    /**
+     * @param string $format
+     * @param bool $allowNilBegin
+     * @return string
+     */
+    public static function generateNumeric($format, $allowNilBegin = false)
+    {
+        $numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $mapping = [];
+        $formatLen = strlen($format);
+        $out = '';
+
+        if ($allowNilBegin === false) {
+            array_splice($numeric, 0, 1);
+        }
+
+        for ($i = 0; $i < $formatLen; $i++) {
+            if (!isset($mapping[$format[$i]])) {
+                $mapping[$format[$i]] = self::randomPullFromArray($numeric);
+            }
+            if ($i === 0 && $allowNilBegin === false) {
+                array_push($numeric, 0);
+            }
+            $out .= $mapping[$format[$i]];
+        }
+        return $out;
+    }
+
+    public static function randomPullFromArray(array &$arr)
+    {
+        $arrLimit = count($arr) - 1;
+        if ($arrLimit === -1) {
+            return null;
+        }
+        if ($arrLimit === 0) {
+            $result = $arr[0];
+            unset($arr[0]);
+            return $result;
+        }
+        $index = rand(0, $arrLimit);
+        $result = $arr[$index];
+        array_splice($arr, $index, 1);
+        return $result;
+    }
 
 
 }
--- test.php
+++ test.php
@@ -2,7 +2,16 @@
 
 require 'vendor/autoload.php';
 
-$bool = \Qbhy\LikeString\LikeString::FormatFullMatch('qbhy', 'qbh7') ? 'true' : 'false';
-print_r($bool);
+use \Qbhy\LikeString\LikeString;
+
+$bool = LikeString::formatFullMatch('qbhy', 'qbh7') ? 'true' : 'false';
+//print_r($bool);
+
+$numeric = [];
+for ($i = 0; $i < 100; $i++) {
+    $numeric[] = LikeString::generateNumeric('aabbccdd');
+}
+
+print_r($numeric);
 
 
